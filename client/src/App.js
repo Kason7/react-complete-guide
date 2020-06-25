1c6a62406e8ba3f4579566bc1a90fc591cfe35ca
Kasper Sonne <kaspersonne27@gmail.com>
2020-06-26 00:22:40 +0200

    Section 3: Understanding the Base Features and Syntax

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+import './Person/Person.css';
+
+const App = () => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+  });
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+
+  const nameChangedHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hi I am a React App</h1>
+      <p>This is really working!</p>
+      <button style={style} onClick={switchNameHandler}>
+        Switch name
+      </button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        changed={nameChangedHandler}
+      >
+        My hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/Person/Person.js
@@ -0,0 +1,15 @@
+import React, { children } from 'react';
+
+export const Person = (props) => {
+  return (
+    <div className='Person'>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old.
+      </p>
+      <p>{props.children}</p>
+      <input type='text' onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
+
+export default Person;
